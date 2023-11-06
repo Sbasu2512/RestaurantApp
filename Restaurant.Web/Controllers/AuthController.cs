faba3c6410c334a31b25114fe3f7b9b6fa324f55
Sayantan Basu <sayantanbasu@live.in>
2023-11-06 14:22:00 -0500

    UI configured for register and login

---
+++ Restaurant.Web/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurant.Web.Models;
+using Restaurant.Web.Service.IService;
+
+namespace Restaurant.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        //dependency injection being consumed here ==> injecting auth service for  login/register.
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new();
+            return View(loginRequestDto);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
